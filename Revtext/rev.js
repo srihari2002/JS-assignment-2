909b861a8abb692a01e264e6287facbea55c77a4
srihari2002 <76547502+srihari2002@users.noreply.github.com>
2021-03-27 21:51:15 +0530

    Delete Revtext directory

--- Revtext/rev.js
+++
@@ -1,6 +0,0 @@
-function revStr(){   
-    var string=document.getElementById("text").value;
-    let strRev=string.split("").reverse().join("");
-    
-    document.getElementById("result").innerHTML= strRev;
-}
